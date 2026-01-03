e3b1521e44ae29f8f896098e4da94adef6f3196f
balazsparej <canadianbali23@gmail.com>
2026-01-03 17:02:16 +0100

    add redisconfig

---
+++ src/main/java/balazs/parej/ticketing/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package balazs.parej.ticketing.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+    
+    @Value("${spring.redis.host:localhost}")
+    private String redisHost;
+    
+    @Value("${spring.redis.port:6379}")
+    private int redisPort;
+    
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+            .setAddress("redis://" + redisHost + ":" + redisPort)
+            .setConnectionPoolSize(10)
+            .setConnectionMinimumIdleSize(5)
+            .setConnectTimeout(3000)
+            .setTimeout(3000)
+            .setRetryAttempts(3)
+            .setRetryInterval(1500);
+        
+        return Redisson.create(config);
+    }
+}
